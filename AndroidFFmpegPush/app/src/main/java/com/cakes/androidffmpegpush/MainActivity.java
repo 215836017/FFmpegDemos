b8492f80e40e8fb8d5c018a0a086b06edd1db857
215836017 <215836017@qq.com>
2021-05-04 10:16:13 +0800

    完成推流Demo的编码，还未测试

--- AndroidFFmpegPush/app/src/main/java/com/cakes/androidffmpegpush/MainActivity.java
+++ AndroidFFmpegPush/app/src/main/java/com/cakes/androidffmpegpush/MainActivity.java
@@ -3,9 +3,19 @@ package com.cakes.androidffmpegpush;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
+    /*** 要推送的目的地址 */
+    private final String DEST_URL = "";
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -20,6 +30,38 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        findViewById(R.id.btn_push_video).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPushVideo();
+            }
+        });
+    }
+
+    private void startPushVideo() {
+        String dir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/testFFmpeg/";
+        File dirFile = new File(dir);
+        if (null != dirFile) {
+            if (!dirFile.exists()) {
+                dirFile.mkdirs();
+            }
+        }
+
+        LogUtil.d(TAG, "path = " + dir + "srcVideo.mp4");
+        File srcFile = new File(dir + "srcVideo.mp4");
+        if (null == srcFile || !srcFile.exists()) {
+            showToast("源文件不存在或文件错误");
+            return;
+        }
+
+        LogUtil.d(TAG, "srcFile.getAbsolutePath() = " + srcFile.getAbsolutePath());
+        showToast("开始推送了...");
+        pushVideo(srcFile.getAbsolutePath(), DEST_URL);
+    }
+
+    private void showToast(String msg) {
+        LogUtil.i(TAG, msg);
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 
     /**
@@ -27,4 +69,6 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    private native int pushVideo(String srcPath, String destUrl);
 }
