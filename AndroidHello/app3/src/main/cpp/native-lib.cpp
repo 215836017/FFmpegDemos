39b158e6b9715b0a8820a0072a6f6300d7278fe6
215836017 <215836017@qq.com>
2021-09-15 19:40:02 +0800

    学习CMakeLists.txt引用第三方库的第三种写法

---
+++ AndroidHello/app3/src/main/cpp/native-lib.cpp
@@ -0,0 +1,37 @@
+#include <jni.h>
+#include <string>
+
+extern "C" {
+#include <libavcodec/version.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/version.h>
+#include <libavutil/version.h>
+#include <libavfilter/version.h>
+#include <libswresample/version.h>
+#include "libavformat/avformat.h"
+#include <libswscale/version.h>
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_cakes_app3_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
+
+    char strBuffer[1024 * 4] = {0};
+    strcat(strBuffer, "CMakeLists.txt引用第三方库的第三种写法：\nlibavcodec: ");
+    strcat(strBuffer, AV_STRINGIFY(LIBAVCODEC_VERSION));
+    strcat(strBuffer, "\nlibavformat : ");
+    strcat(strBuffer, AV_STRINGIFY(LIBAVFORMAT_VERSION));
+    strcat(strBuffer, "\nlibavutil : ");
+    strcat(strBuffer, AV_STRINGIFY(LIBAVUTIL_VERSION));
+    strcat(strBuffer, "\nlibavfilter : ");
+    strcat(strBuffer, AV_STRINGIFY(LIBAVFILTER_VERSION));
+    strcat(strBuffer, "\nlibswresample : ");
+    strcat(strBuffer, AV_STRINGIFY(LIBSWRESAMPLE_VERSION));
+    strcat(strBuffer, "\nlibswscale : ");
+    strcat(strBuffer, AV_STRINGIFY(LIBSWSCALE_VERSION));
+    strcat(strBuffer, "\navcodec_configure : \n");
+    strcat(strBuffer, avcodec_configuration());
+    strcat(strBuffer, "\navcodec_license : ");
+    strcat(strBuffer, avcodec_license());
+
+    return env->NewStringUTF(strBuffer);
+}
